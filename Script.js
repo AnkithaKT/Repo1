aabe855eafc0ae86cc2b9da7c0eac2c832509cb1
AnkithaKT <72155365+AnkithaKT@users.noreply.github.com>
2020-10-14 16:01:05 +0530

    JavaScript

---
+++ Script.js
@@ -0,0 +1,19 @@
+// Wait till the DOM is ready.
+$(function () {
+  var roles = ["Student", "Reader", "Art Enthusiast"];
+  var count = 0;
+  setInterval(() => {
+    $("#intro .content h2 span").fadeOut("fast", function () {
+      $(this).text(roles[count++ % roles.length]);
+      $(this).fadeIn("fast");
+    });
+  }, 1000);
+  $(".navbar").hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 40) {
+      $(".navbar").slideDown(100);
+    } else {
+      $(".navbar").slideUp(100);
+    }
+  });
+});
